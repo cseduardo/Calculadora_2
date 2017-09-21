717172eb6e1adc489ae0b87b9bd9c6d341f89a77
Eduardo Campos <iwame@DESKTOP-UM7N6BP>
2017-09-20 22:30:28 -0500

    Add project files.

---
+++ Calculadora_2/Calculadora_2/MainPage.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculadora_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public static Int32 dato1 = new Int32();
+        public static Int32 dato2 = new Int32();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void porciento_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void raiz_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cuadrado_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inversa_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void borrar_todo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void borrar_ultimo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void borrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siete_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ocho_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nueve_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mult_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cuatro_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cinco_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seis_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uno_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tres_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void suma_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void absoluto_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cero_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void punto_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void igual_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
